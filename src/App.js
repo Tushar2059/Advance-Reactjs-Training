b604d2544d5cbf85144369991b5fea4b933654e9
Tushar2059 <Tushar2059>
2022-09-11 11:56:59 +0530

    working with getting values from input field

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import {useRef} from "react"
 function App() {
+ var ref2=useRef();
+
   let ref = { id: 2, username: "Tushar" };
   let list = ["pune", "mumbai", "nagpur", "kolkata", "delhi"];
   let ref1 = [
@@ -8,9 +11,11 @@ function App() {
     { id: 4, username: "Sandip",active:false },
   ];
 
-  const delete1 = (e) => {
-    console.log(e);
+  const handle = (e) => {
+    console.log(ref2.current.value);
   };
+
+
 
   return (
     <div>
@@ -30,6 +35,13 @@ function App() {
         (<h1>{item.id}. {item.username}</h1>)
         )
       }
+
+     <h1>Getting values from input field</h1>
+
+     <input ref={ref2} type="text" ></input>
+     <input type="submit" value="enter name" onClick={handle} ></input>
+
+
     </div>
   );
 }
