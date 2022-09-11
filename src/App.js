61eff830f6882c9a54af3bc0e7cf8b4fa35cd36d
Tushar2059 <Tushar2059>
2022-09-11 11:45:29 +0530

    filter and map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  let ref = { id: 2, username: "Tushar" };
+  let list = ["pune", "mumbai", "nagpur", "kolkata", "delhi"];
+  let ref1 = [
+    { id: 1, username: "Tushar",active:true },
+    { id: 2, username: "Rohit",active:true },
+    { id: 3, username: "Amit",active:true },
+    { id: 4, username: "Sandip",active:false },
+  ];
+
+  const delete1 = (e) => {
+    console.log(e);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{ref.username}</p>
+      <h2>Map</h2>
+      {list.map((item, index) => (
+        <>
+          <h3>{item}</h3>
+         
+        </>
+      ))}
+
+
+      <h2>Filter and Map chaining</h2>
+      {
+        ref1.filter((item)=>item.active).map((item,index)=>
+        (<h1>{item.id}. {item.username}</h1>)
+        )
+      }
     </div>
   );
 }
